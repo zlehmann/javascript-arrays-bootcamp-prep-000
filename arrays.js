dc1697dccdfe01d104392c960a1807c9dc6066ef
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-24 21:28:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray(arr, element) {
   arr.unshift(element)
   return arr
 }
+
+function addElementToEndOfArray(arr, element){
+  var newArray = [...arr, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr.push(element)
+  return arr
+}
