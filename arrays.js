accf84d7086da8d803bc3488c75f66c51892c2e3
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-24 21:14:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,5 @@
-var chocolateBars = []
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(arr, element) {
+  var newArray = [element, ...arr]
+}
